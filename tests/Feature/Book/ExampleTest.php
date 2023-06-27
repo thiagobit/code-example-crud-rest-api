1dd5afb5ec8115864df7c29d9491fc172a987fc6
Thiago Bittencourt <thiago.bittencourt@chroniclestudio.com>
2023-06-26 23:48:50 -0300

    organizing feature tests in directories

--- tests/Feature/Book/ExampleTest.php
+++ tests/Feature/Book/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\Book;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
--- tests/Feature/Book/ShowBookTest.php
+++ tests/Feature/Book/ShowBookTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature;
+namespace Tests\Feature\Book;
 
 use App\Models\Book;
 use Illuminate\Foundation\Testing\RefreshDatabase;
